a78753fe2adf6ce53b96f63e7881562e4b7f2950
kusalmagar <kusal.mgr33@gmail.com>
2021-06-09 19:56:15 +0545

    api url updated

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   final String worldBaseURl = "https://corona.lmao.ninja/v2/all";
   final String countryBaseUrl =
-      "https://corona.lmao.ninja/v2/countries?sort=cases";
+      "https://disease.sh/v3/covid-19/countries?sort=cases";
   final String countryCasesBaseURL = "https://disease.sh/v2/countries/";
 }
